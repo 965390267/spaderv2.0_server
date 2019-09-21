5e2644c6ac40fb601c5aa8be1ba3fc2d2c6ee51e
zzh965390267 <965390267@qq.com>
2019-09-21 18:38:40 +0800

    sdfsd

--- spaderMain/spader.js
+++ spaderMain/spader.js
@@ -1,5 +1,5 @@
 var schedule = require('node-schedule');//定时任务模块
-var request = require('../utils/request.js');//请求库模块
+var fetch=require('./fetch')
 var conf=require('./rule')//爬虫任务列表，直接从本地内存中取，避免不必要的读取操作数据库，只有提交了爬虫规则的时候才从数据库读出更新任务列表
 var analysis=require('./ruleanalysis')//对爬虫输入的规则的参数进行处理，
 function spader() {
@@ -16,13 +16,13 @@ console.log('start');
     var times1    = [1,12,24,34,42,52,59];  
     rule1.second  = times1;  
     // schedule.scheduleJob(rule1, function(){  
-      let promiseAll=[]
-      for (let index = 0; index < conf.length; index++) {//循环爬取所有的任务列表，因为是基于事件循环机制，所以不会导致线程阻塞     
-       promiseAll.push(request(conf[index].charset, conf[index].site))
-      }
-      Promise.all(promiseAll).then(res=>{
+      fetch().then((res)=>{
         for (let index = 0; index < res.length; index++) {
-          analysis(res[index],conf[index])        
+          analysis(res[index],conf[index],function(result){
+ console.log(result);
+ 
+          })
+          
         }
       })
     // });  
