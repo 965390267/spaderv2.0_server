aa526ee0dae8ee9ddbcd05d45e8a186e798183a6
zzh965390267 <965390267@qq.com>
2019-09-21 23:43:43 +0800

    23

--- spaderMain/rule.js
+++ spaderMain/rule.js
@@ -1,5 +1,5 @@
-let conf = [{ site: 'http://www.cxrs.gov.cn/cxnew/list-60.html', MainSelector: 'body > div.mbd > div > div.pleft > ul > li', TimeSelector: 'div', HrefSelector: 'a', charset: 'utf' },
-{ site: 'http://www.rsj.yuxi.gov.cn/renshe/article.g?m=listRenshe&catecode=100010035', MainSelector: 'body > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > table > tbody > tr:nth-child(4) > td > table > tbody > tr > td:nth-child(2) > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div > ul', TimeSelector: 'li > span > span > font', HrefSelector: 'li > a', charset: 'gbk' }
+let conf = [{ site: 'http://www.cxrs.gov.cn/cxnew/list-60.html', MainSelector: 'jjjjjjj', TimeSelector: 'div', HrefSelector: 'a', charset: 'utf' },
+
 ]
 /* 爬虫规则组，每间隔一段时间从这里取规则，只需在每次添加规则的时候在重新读取数据库的爬虫规则到这儿，避免多次操作数据库 */
 let time = 5;
--- spaderMain/spader.js
+++ spaderMain/spader.js
@@ -1,5 +1,5 @@
 var schedule = require('node-schedule');//定时任务模块
-var fetch=require('./fetch')
+var fetch=require('./fetch');//所有请求的任务同步返回
 var conf=require('./rule')//爬虫任务列表，直接从本地内存中取，避免不必要的读取操作数据库，只有提交了爬虫规则的时候才从数据库读出更新任务列表
 var analysis=require('./ruleanalysis')//对爬虫输入的规则的参数进行处理，
 function spader() {
@@ -8,9 +8,6 @@ console.log('start');
   //  var rule2  = new schedule.RecurrenceRule();  //定时启动爬虫开始抓取
   // var times2    = [1,10,20,30,40,50,60];  
   // rule2.minute  = times2;  
-  //  schedule.scheduleJob(rule2, function(){   
-  //   console.log(conf);
-  //  }); 
     //每分钟的第30秒定时执行一次:
     var rule1     = new schedule.RecurrenceRule();  
     var times1    = [1,12,24,34,42,52,59];  
@@ -19,17 +16,12 @@ console.log('start');
       fetch().then((res)=>{
         for (let index = 0; index < res.length; index++) {
           analysis(res[index],conf[index],function(result){
- console.log(result);
- 
-          })
-          
+              console.log(result);
+          })     
         }
       })
-    // });  
-  
+    // });   
 }
-
-
 
 // request('gbk', 'http://www.ynqjrs.cn/index.asp?Twzbh=1', function (sres) {
 
